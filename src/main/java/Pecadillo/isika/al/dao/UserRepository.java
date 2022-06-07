76e58e92d248cbc6cbc27a3aa1aab316b70b51f3
Vallagarde <val.lagarde29@gmail.com>
2022-06-07 20:12:24 +0200

    adding repository for User and Role + beginning implementation of websecurity conf and jwt

---
+++ src/main/java/Pecadillo/isika/al/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package Pecadillo.isika.al.dao;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import Pecadillo.isika.al.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+	Boolean existsByUsername(String username);
+	Boolean existsByEmail(String email);
+}
